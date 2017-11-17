c85d783d7b910c430341151a611f6f6e576f925f
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-17 14:22:06 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -18,9 +18,8 @@ class Question{
     const self = this
     return new Promise(function(resolve){
       const sql = `INSERT INTO questions (content) VALUES (?)`
-      
+
       db.run(sql, self.content, function(){
-        console.log(`...question ${this.lastID} inserted into database`)
         self.id = this.lastID
         resolve(self)
       })
